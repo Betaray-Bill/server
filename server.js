09d5990a8538ac897a3f93b54f223ab89db252c1
Betaray-Bill <suryaorion12@gmail.com>
2025-07-03 00:28:02 +0530

    asc

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 const corsOptions = {
     origin: [
-        "http://localhost:5173",
+        "*",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
